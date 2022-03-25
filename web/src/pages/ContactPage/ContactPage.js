40ac7c99ea69e4b466efb5712608a371a8e0c8cb
Shane Kunz <shanemkunz@gmail.com>
2022-03-25 11:58:24 -0500

    chakra with redwood forms code

--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -1,16 +1,23 @@
-import { MetaTags } from '@redwoodjs/web'
+import { MetaTags, useMutation } from '@redwoodjs/web'
 import {
   FieldError,
   Form,
-  Label,
   TextField,
   TextAreaField,
   Submit,
   FormError,
   useForm,
 } from '@redwoodjs/forms'
-import { useMutation } from '@redwoodjs/web'
 import { toast, Toaster } from '@redwoodjs/web/toast'
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea,
+  FormErrorMessage,
+  Container,
+} from '@chakra-ui/react'
 
 const CREATE_CONTACT = gql`
   mutation CreateContactMutation($input: CreateContactInput!) {
@@ -37,47 +44,53 @@ const ContactPage = () => {
     <>
       <MetaTags title="Contact" description="Contact page" />
       <Toaster />
-      <Form
-        onSubmit={onSubmit}
-        config={{ mode: 'onBlur' }}
-        error={error}
-        formMethods={formMethods}
-      >
-        <FormError error={error} wrapperClassName="form-error" />
-        <Label name="name" errorClassName="error">
-          Name
-        </Label>
-        <TextField
-          name="name"
-          validation={{ required: true }}
-          errorClassName="error"
-        />
-        <FieldError name="name" className="error" />
-
-        <Label name="email" errorClassName="error">
-          Email
-        </Label>
-        <TextField
-          name="email"
-          validation={{
-            required: true,
-          }}
-          errorClassName="error"
-        />
-        <FieldError name="email" className="error" />
-
-        <Label name="message" errorClassName="error">
-          Message
-        </Label>
-        <TextAreaField
-          name="message"
-          validation={{ required: true }}
-          errorClassName="error"
-        />
-        <FieldError name="message" className="error" />
-
-        <Submit disabled={loading}>Save</Submit>
-      </Form>
+      <Container>
+        <Form
+          onSubmit={onSubmit}
+          config={{ mode: 'onBlur' }}
+          error={error}
+          formMethods={formMethods}
+        >
+          <FormError error={error} wrapperClassName="form-error" />
+          <FormControl>
+            <FormLabel htmlFor="name">Name</FormLabel>
+            <Input
+              as={TextField}
+              name="name"
+              validation={{ required: true }}
+              errorClassName="error"
+            />
+            <FormErrorMessage as={FieldError} name="name" className="error" />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="email">Email address</FormLabel>
+            <Input
+              as={TextField}
+              name="email"
+              validation={{ required: true }}
+              errorClassName="error"
+            />
+            <FormErrorMessage as={FieldError} name="email" className="error" />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="message">Message</FormLabel>
+            <Textarea
+              as={TextAreaField}
+              name="message"
+              validation={{ required: true }}
+              errorClassName="error"
+            />
+            <FormErrorMessage
+              as={FieldError}
+              name="message"
+              className="error"
+            />
+          </FormControl>
+          <Button as={Submit} type="submit" disabled={loading} mt={4}>
+            Save
+          </Button>
+        </Form>
+      </Container>
     </>
   )
 }
